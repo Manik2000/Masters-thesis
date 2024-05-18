12f4a18c3462c9312866497beb5a2830ca99636e
Marcin Kostrzewa <Manik24901@gmail.com>
2024-05-18 16:47:23 +0200

    added structure for the project

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+
+def read_file(file_path):
+    with open(file_path, "r") as file:
+        return file.read()
+
+setup(
+    name="trajectory_image_dataset",
+    version="0.0.1",
+    packages=find_packages('src'),
+    author="Marcin Kostrzewa",
+    author_email="Manik24901@gmail.com",
+    description="A package for CPD and classfication of anomalous diffusion trajectories.",
+    long_description=read_file("README.md")
+)
